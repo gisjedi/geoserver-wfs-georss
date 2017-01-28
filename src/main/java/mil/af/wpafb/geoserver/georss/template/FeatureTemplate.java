2b34bb586c6342f6c39c41ac4b3ecd4791f0dd31
Jonathan Meyer <jon@gisjedi.com>
2017-01-28 16:59:26 +0000

    Fixed typo in package statement

--- src/main/java/mil/af/wpafb/geoserver/georss/template/FeatureTemplate.java
+++ src/main/java/mil/af/wpafb/geoserver/georss/template/FeatureTemplate.java
@@ -2,7 +2,7 @@
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
-package mil.af.wpafb.geoserver.ageorss.template;
+package mil.af.wpafb.geoserver.georss.template;
 
 import java.io.CharArrayWriter;
 import java.io.IOException;
